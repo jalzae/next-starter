c4445afb25a642d14b375da56dad101dc2c85a48
JalzAe <jalzae@gmail.com>
2023-09-07 10:42:59 +0700

    update setting ssr:false

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  target: "serverless",
+  ssr: false,
+  async rewrites() {
+    return [
+      // Rewrite everything to `pages/index`
+      {
+        source: "/:any*",
+        destination: "/",
+      },
+    ];
+  },
+}
 
 module.exports = nextConfig
+
